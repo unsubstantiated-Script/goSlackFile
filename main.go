700a082f95f9191c67546104d17ab7c9083cf132
unsubstantiated-Script <jusfulton@gmail.com>
2024-06-17 19:27:45 -0700

    Main app finished
    Few hitches due to a deprecated method, but got it sorted out and working now. Huzzah for great GoLang documentation. That's super nice.

--- main.go
+++ main.go
@@ -3,12 +3,39 @@ package main
 import (
 	"fmt"
 	"github.com/joho/godotenv"
+	"github.com/slack-go/slack"
 	"os"
 )
 
 func main() {
 	slackBotToken := goDotEnvVariable("SLACK_BOT_TOKEN")
 	channelID := goDotEnvVariable("CHANNEL_ID")
+
+	api := slack.New(slackBotToken)
+
+	fileInfo, err := os.Stat("Testing_Jazz.pdf")
+
+	if err != nil {
+		fmt.Printf("Error parsing file: %s\n", err)
+		return
+	}
+
+	//Flattened this out of an array as prior technique didn't factor in new method and its params.
+	params := slack.UploadFileV2Parameters{
+		Channel:  channelID,
+		File:     fileInfo.Name(),
+		Filename: fileInfo.Name(),
+		FileSize: int(fileInfo.Size()),
+	}
+
+	file, err := api.UploadFileV2(params)
+
+	if err != nil {
+		fmt.Printf("Error uploading file: %s\n", err)
+		return
+	}
+
+	fmt.Printf("File uploaded:%s\n", file)
 
 }
 
